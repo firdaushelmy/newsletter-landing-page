851ba6192b567b99257ae857e01d4d83f9e0ddec
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-03 03:16:07 +0800

    fixed bugs from amateur coder

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const request = require('request')
-const https = require('https')
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
 
-const app = express()
+const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+require('dotenv').config();
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/signup.html');
 });
 
 app.post('/', function (req, res) {
-  const firstName = req.body.firstName
-  const lastName = req.body.lastName
-  const emailAdd = req.body.emailAdd
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const emailAdd = req.body.emailAdd;
+  const SERVER_NUMBER = process.env.SERVER_NUMBER;
+  const API_KEY = process.env.API_KEY;
+  const AUDIENCE_LIST_ID = process.env.AUDIENCE_LIST_ID;
 
-  var data = {
+  const data = {
     members: [
       {
         email_address: emailAdd,
@@ -31,7 +35,7 @@ app.post('/', function (req, res) {
   };
 
   const jsonData = JSON.stringify(data);
-  url = `https://us${SERVER_NUMBER}.api.mailchimp.com/3.0/lists/${AUDIENCE_LIST_ID}`;
+  const url = `https://${SERVER_NUMBER}.api.mailchimp.com/3.0/lists/${AUDIENCE_LIST_ID}`;
   const options = {
     method: 'POST',
     auth: `firdausTest:${API_KEY}`
@@ -40,8 +44,8 @@ app.post('/', function (req, res) {
   const request = https.request(url, options, function (response) {
     response.on('data', function (data) {
       console.log(JSON.parse(data));
-    })
-  })
+    });
+  });
   request.write(jsonData);
   request.end();
 })

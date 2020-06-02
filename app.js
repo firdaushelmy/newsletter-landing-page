a45360bb61a47bbe05edb768a98fd491ead3ee46
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-02 23:26:09 +0800

    filled HTML, css, and images > followed by adding the static assets into public folder

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const request = require('request')
 
 const app = express()
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/signup.html')
-})
+  res.sendFile(__dirname + '/signup.html');
+});
 
 app.listen(3000, function () {
-  console.log('server 3000 listening port is running')
+  console.log('server 3000 listening port is running');
 });
